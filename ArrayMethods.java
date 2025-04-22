724473962bcffffd8f7b638664c08126a8f68950
Jasmin Leonrodriguez <jleonrodriquez@student.sdccd.edu>
2025-04-22 11:50:48 -0700

    class update

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -31,13 +31,44 @@ public class ArrayMethods
     public int Max(){//+ findMax (): int 
         int Max = myArray[0];
         int index = 0;
-        for (0<myArray.length)
-        if (myArray[i]>Max0{
+        for (0< myArray.length)
+        if (myArray[i]>Max0){
             Max = myArray[i];
             index = i;
         }
-        
-        
-        
+
         return Max; // short cut retun (double) sum()/count();    
     }
+
+    public int findLast(int key){//+findLast (int key): int
+        for (int c = myArray.length-1;i >=0;i--)
+            if(myArray[i]==key){
+                int index=-1;
+                return-1;
+            }
+    }
+
+    public int[] findAll (int key){//+findAll(int key): int [ ] 
+        int counter =0;
+        for(int i=0; i< myArray.length; i++){
+            if(myArray [i] ==key)
+                counter ++;
+        }
+
+    }
+
+    public int[] getArray(){//+getArray(): int[ ]
+        int[]array= new int[counter];
+        int index = 0;
+        for(int i=0; i< myArray.length; i++)
+            if(myArray[i] ==key){
+            array[index]=i;
+            counter ++;
+        }
+        return array;
+    }
+    
+    public int[] copyArray(){//+copyArray (): int [ ] 
+        arrays.copyof(myArray, myArray.length);
+    }
+}
