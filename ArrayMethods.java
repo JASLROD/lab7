9cfbc8d5ecee3d18a3e23def39999a4e73a304b1
Jasmin Leonrodriguez <jleonrodriquez@student.sdccd.edu>
2025-04-24 08:56:54 -0700

    home edits

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -2,8 +2,9 @@
 /**
  * Write a description of class ArrayMethods here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
+ * @version v1.0
+ * @since 4/22/2025
  */
 public class ArrayMethods
 {
@@ -11,15 +12,15 @@ public class ArrayMethods
 
     public int count(){
         int size = 0;
-        for (int number;myArray)
-        size++;
+        for (int number: myArray)
+            size++;
         return size;
     }
 
     public int sum(){
         int total = 0;
-        for (int number;myArray)
-        total+=number;
+        for (int number: myArray)
+            total+=number;
         return total;
     }
 
@@ -28,47 +29,80 @@ public class ArrayMethods
         return average; // short cut retun (double) sum()/count();
     }
 
-    public int Max(){//+ findMax (): int 
+    public int findMax(){//+ findMax (): int 
         int Max = myArray[0];
-        int index = 0;
-        for (0< myArray.length)
-        if (myArray[i]>Max0){
-            Max = myArray[i];
-            index = i;
+        for (int i=1; i< myArray.length; i++){
+            if (myArray[i]>Max){
+                Max = myArray[i];
+            }
         }
 
         return Max; // short cut retun (double) sum()/count();    
     }
 
-    public int findLast(int key){//+findLast (int key): int
-        for (int c = myArray.length-1;i >=0;i--)
-            if(myArray[i]==key){
-                int index=-1;
-                return-1;
+    public int findIndexOfMax(){//+findIndexOfMax (): int
+        int Max = myArray[0];
+        int index = 0;
+        for (int i=1; i< myArray.length; i++){
+            if (myArray[i]>Max){
+                Max = myArray[i];  
+                index = i;
             }
+        }
+        return index;
+    }
+
+    public int findLast(int key){//+findLast (int key): int
+        int Last = -1;
+        int i = myArray.length-1;
+        boolean found = false;
+        while(!found && i>=0){
+            if(myArray[i]==key){
+                found = true;
+                Last = i;
+            }
+            int index=-1;
+        }
+        return Last;
     }
 
     public int[] findAll (int key){//+findAll(int key): int [ ] 
         int counter =0;
-        for(int i=0; i< myArray.length; i++){
-            if(myArray [i] ==key)
+        for(int i=0; i< myArray.length; i++)
+            if(myArray [i] ==key){
                 counter ++;
+            }
+        int array[] = new int[counter];
+        int index = 0;
+
+        for(int i=0; i<myArray.length; i++){
+            if(myArray[i] == key){
+                array[index]=i;
+                index++;
+            }
         }
 
+        return array;
     }
 
     public int[] getArray(){//+getArray(): int[ ]
-        int[]array= new int[counter];
-        int index = 0;
-        for(int i=0; i< myArray.length; i++)
-            if(myArray[i] ==key){
-            array[index]=i;
-            counter ++;
-        }
-        return array;
+
+        return myArray;
     }
-    
+
     public int[] copyArray(){//+copyArray (): int [ ] 
-        arrays.copyof(myArray, myArray.length);
+        int copy[] = Arrays.copyOf(myArray, myArray.length);
+        return copy;
+    }
+
+    public void print(int[] inArray){//+print(int [ ] a): void
+        System.out.print("{");
+        int i;
+        for(i =0 ; i < inArray.length-1; i++)
+            System.out.print(inArray[i] +",");
+        if (inArray.length> 0 )
+            System.out.print(inArray[i]);
+        System.out.println("}");
     }
 }
+
